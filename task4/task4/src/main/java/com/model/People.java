f516911778fec455bffd7c86a86fc59610d67e3e
zhuojian <2190755272@qq.com>
2018-07-30 20:53:23 +0800

    任务四

---
+++ task4/task4/src/main/java/com/model/People.java
@@ -0,0 +1,77 @@
+package com.model;/*
+ * @ClassName:People
+ * @Description:学员类
+ * @Author qiao
+ * @Date 2018/7/24 20:31
+ **/
+
+import java.util.Date;
+
+public class People {
+    private long id;
+    private String name;
+    private String info;
+    private long creatTime;
+    private long updateTime;
+    private int job;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public long getCreatTime() {
+        return creatTime;
+    }
+
+    public void setCreatTime(long creatTime) {
+        this.creatTime = creatTime;
+    }
+
+    public long getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(long updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public int getJob() {
+        return job;
+    }
+
+    public void setJob(int job) {
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", info='" + info + '\'' +
+                ", creatTime=" + creatTime +
+                ", updateTime=" + updateTime +
+                ", job=" + job +
+                '}';
+    }
+}
